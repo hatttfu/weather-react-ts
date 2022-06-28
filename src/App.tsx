8bf4fa6d1c9f5c6a6b4d9cd9cdefb9327ced42e1
galiya <gkey-587@mail.ru>
2022-06-28 13:41:08 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import searchIcon from './images/search.png';
+import sunIcon from './images/sun.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+          <header className="header">
+              the.weather
+          </header>
+          <main className="main">
+              <div className="info-block">
+                  <div className="temperature">52°</div>
+                  <div className="place-time">
+                      <div className="place">London</div>
+                      <div className="time-info">
+                          <div className="time">10:36 </div>-
+                          <div className="weekday">Tuesday</div>
+                          <div className="date">22 Oct '19</div>
+                      </div>
+                  </div>
+                  <div className="description">
+                      <div className="icon">
+                          <img src={sunIcon} alt="" />
+                      </div>
+                      <div className="icon-info">
+                          Sunny
+                      </div>
+                  </div>
+              </div>
+          </main>
+          <div className="search">
+              <button className="search-button">
+                  <img src={searchIcon} alt="Check weather" />
+              </button>
+              <div className="form">
+                  <input type="search" placeholder="Type city here..." className="search-town" />
+                  <div className="details">
+                      <h3>Weather Details</h3>
+                      <div className="weather-block">
+                          <div>
+                              <div className="name">Cloudy</div>
+                              <div className="value">12%</div>
+                          </div>
+                          <div>
+                              <div className="name">Cloudy</div>
+                              <div className="value">12%</div>
+                          </div>
+                          <div>
+                              <div className="name">Cloudy</div>
+                              <div className="value">12%</div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
     </div>
   );
 }
